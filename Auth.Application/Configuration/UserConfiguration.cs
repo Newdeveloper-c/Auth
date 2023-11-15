ccf626c777b66453d028330c88c46619fc81c34e
Newdeveloper-c <turdimurodovnumonjon@gmail.com>
2023-11-15 18:44:02 +0500

    some configurations added

---
+++ Auth.Application/Configuration/UserConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Auth.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JWT.Configurations;
+
+internal sealed class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("Users");
+
+        builder.HasKey(x => x.Id);
+
+        builder.HasIndex(x => x.Email).IsUnique();
+    }
+}
